4d15c1a0a987edcb74f00022cd5895159fe642dd
choisuhyeok1255 <csh300029@gmail.com>
2021-05-13 01:44:09 +0900

    Feat: KaKaoMap 컴포넌트 생성
    	modified:   public/index.html
    	new file:   src/components/KaKaoMap/KaKaoMap.js
    	modified:   src/components/index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,8 @@
 export { default as Button } from "./Button/Button";
 export { default as Heading } from "./Heading/Heading";
 export { default as Container } from "./Container/Container";
+export { default as LinkToWebsite } from "./LinkToWebsite/LinkToWebsite";
+export { default as Input } from "./Input/Input";
+export { default as KaKaoMap } from "./KaKaoMap/KaKaoMap";
+export { default as Form } from "./Form/Form";
+export { default as SVGIcon } from "./SVGIcon/SVGIcon";
