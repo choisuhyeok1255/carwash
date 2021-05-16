293abe88240084d7c29079bfd909ea6969ecafed
choisuhyeok1255 <csh300029@gmail.com>
2021-05-16 15:08:24 +0900

    Refactor: 모달창 사용 할 수 있도록 상태 추가

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,8 +1,15 @@
-import { Button, Heading } from "components";
+import { Button, Heading, LinkHeader, Portal } from "components";
 import Container from "components/Container/Container";
-import React from "react";
+import { LoginModalDialog } from "containers";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handlerModalOpen = () => {
+    setIsModalOpen(true);
+  };
+
   return (
     <Container>
       <Container
@@ -10,9 +17,22 @@ const Header = () => {
         $margin="0 auto"
         $justifyContent="space-between"
       >
-        <Button>출석</Button>
-        <Heading>빛나는 하루</Heading>
-        <Button>로그인</Button>
+        <Button $backgroundColor="inherit">출석</Button>
+        <Heading>
+          <LinkHeader to="/">빛나는 하루</LinkHeader>
+        </Heading>
+        <Button $backgroundColor="inherit" onClick={handlerModalOpen}>
+          로그인
+        </Button>
+        {isModalOpen ? (
+          <>
+            <Portal>
+              <LoginModalDialog
+                setIsModalOpen={setIsModalOpen}
+              ></LoginModalDialog>
+            </Portal>
+          </>
+        ) : null}
       </Container>
     </Container>
   );
