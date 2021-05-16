025967637cdc1e5666826c5b7084db9c36bbaf96
choisuhyeok1255 <csh300029@gmail.com>
2021-05-16 15:00:43 +0900

    Refactor: CloseButton 아이콘 추가

--- src/components/SVGIcon/SVGIcon.js
+++ src/components/SVGIcon/SVGIcon.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { ReactComponent as PlusButton } from "assets/PlusButton.svg";
+import { ReactComponent as CloseButton } from "assets/CloseButton.svg";
 
 const SVGIcon = ({ type, $width, $height, ...restProps }) => {
   let SVGIcon = null;
+
   switch (type) {
     default:
     case "PlusButton":
       SVGIcon = PlusButton;
+      break;
+    case "CloseButton":
+      SVGIcon = CloseButton;
       break;
   }
   return <SVGIcon width={$width} height={$height} {...restProps} />;
