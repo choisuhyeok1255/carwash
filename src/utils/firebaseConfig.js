75ffa69b4eb11f9f7ae63e1851d692d073d2e222
choisuhyeok1255 <csh300029@gmail.com>
2021-05-19 19:46:49 +0900

    Refactor: storage import 및 export

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -3,6 +3,7 @@ import firebase from "firebase/app";
 // 데이터베이스, 인증 모듈 불러오기
 import "firebase/firestore";
 import "firebase/auth";
+import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: `${process.env.REACT_APP_FIREBASE_APP_KEY}`,
@@ -15,11 +16,12 @@ const firebaseConfig = {
 };
 
 // Firebase 앱 초기화
-firebase.initializeApp(firebaseConfig);
+export const firebaseInit = firebase.initializeApp(firebaseConfig);
 
 // Firebase 인증, 데이터베이스 객체 생성 (내보내기)
 export const auth = firebase.auth();
 export const db = firebase.firestore();
+export const storage = firebase.storage();
 
 // Firebase 모듈 기본 내보내기
 export default firebase;
