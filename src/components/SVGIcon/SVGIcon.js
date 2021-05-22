e21317047e57b6f6426d2b049eef841ba2f915e6
choisuhyeok1255 <csh300029@gmail.com>
2021-05-22 11:24:20 +0900

    Refactor: icon 이미지 추가

--- src/components/SVGIcon/SVGIcon.js
+++ src/components/SVGIcon/SVGIcon.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { ReactComponent as PlusButton } from "assets/PlusButton.svg";
 import { ReactComponent as CloseButton } from "assets/CloseButton.svg";
+import { ReactComponent as CloseButtonBlack } from "assets/CloseButtonBlack.svg";
 import { ReactComponent as Google } from "assets/Google.svg";
 import { ReactComponent as Kakao } from "assets/Kakao.svg";
 import { ReactComponent as Naver } from "assets/Naver.svg";
+import { ReactComponent as ModifyPen } from "assets/ModifyPen.svg";
+import { ReactComponent as SportsCar } from "assets/SportsCar.svg";
 
 const SVGIcon = ({ type, $width, $height, ...restProps }) => {
   let SVGIcon = null;
@@ -16,6 +19,9 @@ const SVGIcon = ({ type, $width, $height, ...restProps }) => {
     case "CloseButton":
       SVGIcon = CloseButton;
       break;
+    case "CloseButtonBlack":
+      SVGIcon = CloseButtonBlack;
+      break;
     case "Google":
       SVGIcon = Google;
       break;
@@ -24,6 +30,12 @@ const SVGIcon = ({ type, $width, $height, ...restProps }) => {
       break;
     case "Naver":
       SVGIcon = Naver;
+      break;
+    case "ModifyPen":
+      SVGIcon = ModifyPen;
+      break;
+    case "SportsCar":
+      SVGIcon = SportsCar;
       break;
   }
   return <SVGIcon width={$width} height={$height} {...restProps} />;
