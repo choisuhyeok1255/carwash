86f9fdfea400fe662ff32d5def5d94bea4057a6d
choisuhyeok1255 <csh300029@gmail.com>
2021-05-23 00:49:34 +0900

    Refactor: style props 수정
    원래 props를 받아서 css를 설정했는데
    HomePage에서 버튼으로만 사용하기 때문에 css를 기본적으로 설정해주었다.
    	modified:   src/components/LinkToWebsite/LinkToWebsite.js

--- src/components/LinkToWebsite/LinkToWebsite.js
+++ src/components/LinkToWebsite/LinkToWebsite.js
@@ -1,13 +1,24 @@
 import React from "react";
 import styled from "styled-components";
+import { color } from "styles/color";
 
 const StyledLinkToWebsite = styled.a`
-  font-size: ${(props) => props.$fontSize};
+  font-size: 1.5rem;
+  color: ${color.themeMain};
+  background: white;
+  border: 1px solid ${color.themeMain};
+  border-radius: 5px;
+  padding: 10px 15px;
+
+  :hover {
+    background: ${color.themeMain};
+    color: white;
+  }
 `;
 
-const LinkToWebsite = ({ href, $fontSize, children, ...restProps }) => {
+const LinkToWebsite = ({ href, children, ...restProps }) => {
   return (
-    <StyledLinkToWebsite href={href} $fontSize={$fontSize} {...restProps}>
+    <StyledLinkToWebsite href={href} {...restProps}>
       {children}
     </StyledLinkToWebsite>
   );
