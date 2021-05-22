049ca135042cf68d5e42c2a16bc60d29bacdaadc
choisuhyeok1255 <csh300029@gmail.com>
2021-05-23 00:50:59 +0900

    Refactor: LinkToWebsite에 title 속성 추가

--- src/containers/YoutubeAndShop/YoutubeAndShop.js
+++ src/containers/YoutubeAndShop/YoutubeAndShop.js
@@ -6,7 +6,7 @@ const YoutubeAndShop = () => {
     <Container $justifyContent="space-between" $margin="22px 0 0 0">
       <LinkToWebsite
         href={"https://www.youtube.com/channel/UCB3TwWFdgQnu0Q3iLc4ZXLA"}
-        $fontSize="2rem"
+        title="세차영상 유튜브 바로가기"
       >
         세차 영상
       </LinkToWebsite>
@@ -14,7 +14,7 @@ const YoutubeAndShop = () => {
         href={
           "https://theshiningday.com/?NaPm=ct%3Dkoirmwj6%7Cci%3Dcheckout%7Ctr%3Dds%7Ctrx%3D%7Chk%3D00ce3c8abe5fc98310526b120501752573f395f7"
         }
-        $fontSize="2rem"
+        title="세차용품 쇼핑몰 바로가기"
       >
         쇼핑몰
       </LinkToWebsite>
