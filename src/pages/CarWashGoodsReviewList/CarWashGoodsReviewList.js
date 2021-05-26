26ce64adf1f689397adba0df98a26c1ec2780a8a
choisuhyeok1255 <csh300029@gmail.com>
2021-05-26 17:27:57 +0900

    Refactor: 스켈레톤 효과 추가

--- src/pages/CarWashGoodsReviewList/CarWashGoodsReviewList.js
+++ src/pages/CarWashGoodsReviewList/CarWashGoodsReviewList.js
@@ -1,4 +1,4 @@
-import { Container, Heading, Span } from "components";
+import { Container, Heading, SkeletonUI, Span } from "components";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
@@ -19,32 +19,46 @@ const CarWashGoodsReviewList = () => {
     <>
       <Heading $margin="22px 0">용품 후기 리스트</Heading>
       <ol>
-        {goodsReviewList.map((goodsReview, i) => {
-          return (
-            <GoodsReviewList key={i}>
-              <Container
-                $height="40px"
-                $justifyContent="space-between"
-                $alignItems="center"
-              >
-                <Link
-                  to={{
-                    pathname: `/CarWashGoodsReviewPage/${goodsReview.postid}`,
-                    state: {
-                      postid: goodsReview.postid,
-                    },
-                  }}
+        {goodsReviewList.length === 0 ? (
+          <>
+            <SkeletonUI $width="100%" $height="30px" $margin="10px 0 20px 0" />
+            <SkeletonUI $width="100%" $height="30px" $margin="0 0 20px 0" />
+            <SkeletonUI $width="100%" $height="30px" $margin="0 0 20px 0" />
+            <SkeletonUI $width="100%" $height="30px" $margin="0 0 20px 0" />
+            <SkeletonUI $width="100%" $height="30px" $margin="0 0 20px 0" />
+            <SkeletonUI $width="100%" $height="30px" $margin="0 0 20px 0" />
+            <SkeletonUI $width="100%" $height="30px" $margin="0 0 20px 0" />
+            <SkeletonUI $width="100%" $height="30px" $margin="0 0 20px 0" />
+            <SkeletonUI $width="100%" $height="30px" $margin="0 0 20px 0" />
+          </>
+        ) : (
+          goodsReviewList.map((goodsReview, i) => {
+            return (
+              <GoodsReviewList key={i}>
+                <Container
+                  $height="40px"
+                  $justifyContent="space-between"
+                  $alignItems="center"
                 >
-                  <Span>{goodsReview.subject}</Span>
-                </Link>
-                <Container $width="110px" $justifyContent="space-between">
-                  <Span $fontSize="1.2rem">{goodsReview.name}</Span>
-                  <Span $fontSize="1.2rem">{goodsReview.uploadDate}</Span>
+                  <Link
+                    to={{
+                      pathname: `/CarWashGoodsReviewPage/${goodsReview.postid}`,
+                      state: {
+                        postid: goodsReview.postid,
+                      },
+                    }}
+                  >
+                    <Span>{goodsReview.subject}</Span>
+                  </Link>
+                  <Container $width="110px" $justifyContent="space-between">
+                    <Span $fontSize="1.2rem">{goodsReview.name}</Span>
+                    <Span $fontSize="1.2rem">{goodsReview.uploadDate}</Span>
+                  </Container>
                 </Container>
-              </Container>
-            </GoodsReviewList>
-          );
-        })}
+              </GoodsReviewList>
+            );
+          })
+        )}
       </ol>
     </>
   );
