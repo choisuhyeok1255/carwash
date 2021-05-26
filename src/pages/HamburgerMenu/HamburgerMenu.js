1cf6c404c6514b63b83a1a147e984b78c4ce6db4
choisuhyeok1255 <csh300029@gmail.com>
2021-05-26 17:25:22 +0900

    Feat: 메뉴 페이지 추가

---
+++ src/pages/HamburgerMenu/HamburgerMenu.js
@@ -0,0 +1,42 @@
+import { Container, LinkToWebsite } from "components";
+import React from "react";
+import styled from "styled-components";
+import { color } from "styles/color";
+
+const DisableLinkToWebsite = styled(LinkToWebsite)`
+  :hover {
+    cursor: not-allowed;
+  }
+`;
+
+const HamburgerMenu = () => {
+  return (
+    <>
+      <Container $flexFlow="column" $margin="22px 0 0 0">
+        <LinkToWebsite
+          href="https://www.youtube.com/channel/UCB3TwWFdgQnu0Q3iLc4ZXLA"
+          title="세차영상 유튜브 바로가기"
+          $borderTop={`1px solid ${color.themeMain}`}
+        >
+          세차 영상
+        </LinkToWebsite>
+        <LinkToWebsite
+          href={
+            "https://theshiningday.com/?NaPm=ct%3Dkoirmwj6%7Cci%3Dcheckout%7Ctr%3Dds%7Ctrx%3D%7Chk%3D00ce3c8abe5fc98310526b120501752573f395f7"
+          }
+          title="세차용품 쇼핑몰 바로가기"
+        >
+          쇼핑몰
+        </LinkToWebsite>
+        <DisableLinkToWebsite href="#">개발중</DisableLinkToWebsite>
+        <DisableLinkToWebsite href="#">개발중</DisableLinkToWebsite>
+        <DisableLinkToWebsite href="#">개발중</DisableLinkToWebsite>
+        <LinkToWebsite href="https://github.com/choisuhyeok1255">
+          만든이: choisuhyeok
+        </LinkToWebsite>
+      </Container>
+    </>
+  );
+};
+
+export default HamburgerMenu;
