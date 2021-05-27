6f9dc8531dd8c73cd4452cee7c89e1d2e4c5a9d4
choisuhyeok1255 <csh300029@gmail.com>
2021-05-27 23:41:35 +0900

    Refactor: 기본 프로필이미지 추가 및 세차인증 사용후기 사진 좌우 마진 추가

--- src/pages/CarWashProfile/CarWashProfile.js
+++ src/pages/CarWashProfile/CarWashProfile.js
@@ -8,7 +8,7 @@ const CarWashProfile = () => {
   const [certificationImages, setCertificationImages] = useState([]);
   const [goodsReviews, setGoodsReviews] = useState([]);
   const userProfile = useSelector((state) => state.auth.currentUser);
-  const { name, uid } = userProfile;
+  const { name, uid, defaultProfileImage } = userProfile;
 
   useEffect(() => {
     db.collection("certificationImage")
@@ -49,33 +49,61 @@ const CarWashProfile = () => {
   return (
     <>
       <Container $flexFlow="column" $alignItems="center">
-        <SVGIcon type="SportsCar" $width="200" $height="200" />
+        <Image
+          src={defaultProfileImage}
+          alt="프로필사진"
+          $width="200px"
+          $height="200px"
+          $skeletonWidth="200px"
+        />
         <Span $fontSize="2rem">안녕하세요! {name}님</Span>
       </Container>
       <MainHeading
         toPage="/CarWashUserCertificationPage"
         toEdit="/CarWashCertificationEdit"
         $fontSize="2rem"
-        $margin="55px 0 20px 0"
+        $margin="55px 30px 20px 30px"
       >
         {name}님의 세차 인증
       </MainHeading>
-      <Container $flexWrap="wrap" $justifyContent="space-between">
+      <Container
+        $flexWrap="wrap"
+        $justifyContent="space-between"
+        $margin="0 30px"
+      >
         {certificationImages.map((image) => {
-          return <Image src={image.image} $width="110px" $margin="0 0 5px 0" />;
+          return (
+            <Image
+              src={image.image}
+              $width="175px"
+              $skeletonWidth="175px"
+              $margin="0 0 5px 0"
+            />
+          );
         })}
       </Container>
       <MainHeading
         toPage="/CarWashUserGoodsReviewPage"
         toEdit="/CarWashGoodsReviewEdit"
         $fontSize="2rem"
-        $margin="55px 0 20px 0"
+        $margin="55px 30px 20px 30px"
       >
         {name}님의 사용 후기
       </MainHeading>
-      <Container $flexWrap="wrap" $justifyContent="space-between">
+      <Container
+        $flexWrap="wrap"
+        $justifyContent="space-between"
+        $margin="0 30px 80px 30px"
+      >
         {goodsReviews.map((image) => {
-          return <Image src={image.image} $width="110px" $margin="0 0 5px 0" />;
+          return (
+            <Image
+              src={image.image}
+              $width="175px"
+              $skeletonWidth="175px"
+              $margin="0 0 5px 0"
+            />
+          );
         })}
       </Container>
     </>
