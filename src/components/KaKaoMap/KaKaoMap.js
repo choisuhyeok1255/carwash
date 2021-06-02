543af22d6966316b76b0ea60ff223fa7c7fafcaa
choisuhyeok1255 <csh300029@gmail.com>
2021-06-02 22:24:45 +0900

    Refactor: 맵검색 에러 수정
    키워드 검색 후 지도 클릭 후 이동 시 뒤에 검색 전의 맵이 남아 있는 에러 수정(마커 클릭 시 나오는 window창 에러 수정해야됨..)
    	modified:   src/components/KaKaoMap/KaKaoMap.js
    	modified:   src/containers/SearchCarWash/SearchCarWash.js

--- src/components/KaKaoMap/KaKaoMap.js
+++ src/components/KaKaoMap/KaKaoMap.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import { Form, Input } from "components";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const StyledMap = styled.div`
@@ -6,74 +7,114 @@ const StyledMap = styled.div`
   height: ${(props) => props.$height};
 `;
 
-const { kakao } = window;
+const KaKaoMap = ({ $width, $height, ...restProps }) => {
+  const [inputText, setInputText] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+  const [userLat, setUserLat] = useState("");
+  const [userLon, setUserLon] = useState("");
+  const [map, setMap] = useState(null);
 
-const KaKaoMap = ({ $width, $height, searchValue, ...restProps }) => {
+  const { kakao } = window;
+
+  const onChangeHandler = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setSearchValue(inputText);
+    setInputText("");
+  };
+
+  const placesSearchCB = (data, status, pagination) => {
+    if (status === kakao.maps.services.Status.OK) {
+      // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+      // LatLngBounds 객체에 좌표를 추가합니다
+      const bounds = new kakao.maps.LatLngBounds();
+      for (let i = 0; i < data.length; i++) {
+        displayMarker(data[i]);
+        bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+      }
+      // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+      map.setBounds(bounds);
+    }
+  };
+
+  const displayMarker = (place) => {
+    const infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+    // 마커를 생성하고 지도에 표시합니다
+    const marker = new kakao.maps.Marker({
+      map: map,
+      position: new kakao.maps.LatLng(place.y, place.x),
+    });
+    // 마커에 클릭이벤트를 등록합니다
+    kakao.maps.event.addListener(marker, "click", function () {
+      // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+      infowindow.setContent(
+        '<div style="padding:5px;font-size:12px;">' +
+          place.place_name +
+          "</div>"
+      );
+      infowindow.open(map, marker);
+    });
+  };
+
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function success(position) {
+    navigator.geolocation.getCurrentPosition(function (position) {
       const lat = position.coords.latitude;
       const lon = position.coords.longitude;
-
-      const infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
-
       const container = document.getElementById("map");
-
       const options = {
         center: new kakao.maps.LatLng(lat, lon),
         level: 3,
       };
-
+      const userMarkerImgUrl =
+        "https://image.flaticon.com/icons/png/512/1397/1397898.png";
+      const userMarkerSize = new kakao.maps.Size(32, 34);
+      const userMarkerOption = { offset: new kakao.maps.Point(13, 34) };
+      const userMarkerImg = new kakao.maps.MarkerImage(
+        userMarkerImgUrl,
+        userMarkerSize,
+        userMarkerOption
+      );
+      const userMarkerPosition = new kakao.maps.LatLng(lat, lon);
+      const marker = new kakao.maps.Marker({
+        position: userMarkerPosition,
+        image: userMarkerImg,
+      });
       const map = new kakao.maps.Map(container, options);
-      const ps = new kakao.maps.services.Places();
+      marker.setMap(map);
 
-      //badRequest 때문에 input 값이 빈 값일 때 return 시킴
-      if (!searchValue) return;
-      ps.keywordSearch(searchValue, placesSearchCB);
+      setUserLat(lat);
+      setUserLon(lon);
+      setMap(map);
+    });
+  }, []);
 
-      function placesSearchCB(data, status, pagination) {
-        if (status === kakao.maps.services.Status.OK) {
-          // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-          // LatLngBounds 객체에 좌표를 추가합니다
-          const bounds = new kakao.maps.LatLngBounds();
+  useEffect(() => {
+    if (!searchValue) return;
 
-          for (let i = 0; i < data.length; i++) {
-            displayMarker(data[i]);
-            bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
-          }
-
-          // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
-          map.setBounds(bounds);
-        }
-      }
-
-      function displayMarker(place) {
-        // 마커를 생성하고 지도에 표시합니다
-        const marker = new kakao.maps.Marker({
-          map: map,
-          position: new kakao.maps.LatLng(place.y, place.x),
-        });
-
-        // 마커에 클릭이벤트를 등록합니다
-        kakao.maps.event.addListener(marker, "click", function () {
-          // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-          infowindow.setContent(
-            '<div style="padding:5px;font-size:12px;">' +
-              place.place_name +
-              "</div>"
-          );
-          infowindow.open(map, marker);
-        });
-      }
+    const ps = new kakao.maps.services.Places();
+    ps.keywordSearch(searchValue, placesSearchCB, {
+      location: new kakao.maps.LatLng(userLat, userLon),
     });
   }, [searchValue]);
 
   return (
-    <StyledMap
-      id="map"
-      $width={$width}
-      $height={$height}
-      {...restProps}
-    ></StyledMap>
+    <Form onSubmit={submitHandler}>
+      <Input
+        placeholder="세차장을 검색하세요..."
+        $margin="20px 0 6px 0"
+        $padding="5px"
+        $border="1px solid black"
+        $borderRadius="5px"
+        $width="100%"
+        $height="30px"
+        onChange={onChangeHandler}
+        value={inputText}
+      />
+      <StyledMap id="map" $width={$width} $height={$height} {...restProps} />
+    </Form>
   );
 };
 
--- src/containers/SearchCarWash/SearchCarWash.js
+++ src/containers/SearchCarWash/SearchCarWash.js
@@ -1,37 +1,11 @@
-import { Container, Form, Heading, Input, KaKaoMap } from "components";
-import React, { useState } from "react";
+import { Container, Heading, KaKaoMap } from "components";
+import React from "react";
 
 const SearchCarWash = () => {
-  const [inputText, setInputText] = useState("");
-  const [searchValue, setSearchValue] = useState("");
-
-  const onChangeHandler = (e) => {
-    setInputText(e.target.value);
-  };
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    setSearchValue(inputText);
-    setInputText("");
-  };
-
   return (
     <Container $flexFlow="column" $margin="22px 0 0 0">
       <Heading $margin="0 0 0 10px">세차장 검색</Heading>
-      <Form onSubmit={submitHandler}>
-        <Input
-          placeholder="세차장을 검색하세요..."
-          $margin="20px 0 6px 0"
-          $padding="5px"
-          $border="1px solid black"
-          $borderRadius="5px"
-          $width="100%"
-          $height="30px"
-          onChange={onChangeHandler}
-          value={inputText}
-        />
-        <KaKaoMap $width="100%" $height="300px" searchValue={searchValue} />
-      </Form>
+      <KaKaoMap $width="100%" $height="300px" />
     </Container>
   );
 };
